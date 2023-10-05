13ed69b77b8a75aa4e64034fa8d36640a2c6aa69
nespi <nahuelespinolagrativol@gmail.com>
2023-10-05 11:42:52 -0300

    Changed the todo groupbox into a widget, added scrollbar, changed icon

--- mainwindow.py
+++ mainwindow.py
@@ -1,45 +1,43 @@
-from PySide6.QtWidgets import QMainWindow, QLineEdit, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QWidget, QDateEdit, QCheckBox, QGroupBox, QSpacerItem
+from PySide6.QtWidgets import QMainWindow, QLineEdit, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QWidget, QDateEdit, QCheckBox, QGroupBox, QSizePolicy, QScrollArea
 from PySide6.QtGui import QIcon
-from PySide6.QtCore import QDate
+from PySide6.QtCore import QDate, Qt, QSize
 from pathlib import Path
 import datetime
-
 
 class MainWindow(QMainWindow):
     def __init__(self, app):
         super().__init__()
         self.app = app
         self.setWindowTitle("Simple Todo")
-        self.setWindowIcon(QIcon("pyqt/exp/programa_todo/start.png"))
+        self.setWindowIcon(QIcon("pyqt/exp/simple_todo/start.png"))
         central_widget = QWidget()
-        todo_box = QGroupBox("To do:")
+        self.scroll_area = QScrollArea()  # Create a scroll area
+        self.todo_widget = QWidget()
 
         self.task_list = []
         self.task_check_box_list = []
         self.load_tasks()
         
-        ###### ESTO ES UN TEST ##########
-        #self.task_list.append("ejemplo")
-
-        self.save_tasks()
         layout = QVBoxLayout()
 
         new_task_layout = QHBoxLayout()
         new_task_label = QLabel("New task:")
         self.new_task_line_edit = QLineEdit()
-        add_task_button = QPushButton("Add")
-        add_task_button.clicked.connect(self.add_task_button_clicked)
+        self.add_task_button = QPushButton("Add")
+        self.add_task_button.clicked.connect(self.add_task_button_clicked)
+        self.new_task_line_edit.returnPressed.connect(self.add_task_button.click)
 
         new_task_layout.addWidget(new_task_label)
         new_task_layout.addWidget(self.new_task_line_edit)
-        new_task_layout.addWidget(add_task_button)
+        new_task_layout.addWidget(self.add_task_button)
         
         self.task_layout = QVBoxLayout()
         for task in self.task_list:
             task_check_box = QCheckBox(task)
             self.task_layout.addWidget(task_check_box)
-            # Append the task_check_box to the list
             self.task_check_box_list.append(task_check_box)
+
+        self.task_layout.setAlignment(Qt.AlignTop)
 
         for task_check_box in self.task_check_box_list:
             task_check_box.toggled.connect(self.check_box_clicked)
@@ -49,13 +47,16 @@ class MainWindow(QMainWindow):
         done_button.clicked.connect(self.done_button_clicked)
         done_button_layout.addWidget(done_button)
 
+        self.todo_widget.setLayout(self.task_layout)  # Set the group box layout
+
+        # Add the group box to the scroll area
+        self.scroll_area.setWidget(self.todo_widget)
 
         layout.addLayout(new_task_layout)
-        todo_box.setLayout(self.task_layout)
-        layout.addWidget(todo_box)
+        layout.addWidget(self.scroll_area)  # Add the scroll area to the layout
         layout.addLayout(done_button_layout)
         central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)        
+        self.setCentralWidget(central_widget)  
 
     def add_task_button_clicked(self):
         text = self.new_task_line_edit.text()
@@ -110,19 +111,5 @@ class MainWindow(QMainWindow):
         with open(Path(__file__).with_name("todo.txt"), 'r') as file:
             self.task_list = file.read().splitlines()
         print(f"Loaded tasks:{self.task_list}")
-
-
-
-    def delete_checked_checkboxes_old(self):
-        # Iterate over the checkboxes and remove checked ones
-        checkboxes_to_remove = []
-        for checkbox in self.checkboxes:
-            if checkbox.isChecked():
-                checkboxes_to_remove.append(checkbox)
-
-        for checkbox in checkboxes_to_remove:
-            self.layout.removeWidget(checkbox)
-            checkbox.deleteLater()
-            self.checkboxes.remove(checkbox)
 
  
